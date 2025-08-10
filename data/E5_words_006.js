cd443e6122b5491e7cb70962e73ff240802228e3
panda62788 <panda62788@yahoo.co.jp>
2025-08-11 02:29:12 +0900

    Add files via upload

---
+++ data/E5_words_006.js
@@ -0,0 +1,32 @@
+window.wordJPs = [
+  "晴れている",
+  "曇っている",
+  "雨が降っている",
+  "雪が降っている",
+  "霧がかっている",
+  "風が強い",
+  "晴天",
+  "太陽",
+  "雲",
+  "雨/雨が降る",
+  "雨/にわか雨",
+  "雪/雪が降る",
+  "霧",
+  "晴れる"
+];
+window.wordENs = [
+  "sunny",
+  "cloudy",
+  "rainy",
+  "snowy",
+  "foggy",
+  "windy",
+  "sunshine",
+  "the sun",
+  "cloud",
+  "rain",
+  "shower",
+  "snow",
+  "fog",
+  "clearup"
+];
